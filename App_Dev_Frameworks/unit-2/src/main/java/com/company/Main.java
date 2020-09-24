1d92f29b35916ddb00d068a3bb1a29f8a7eddc29
ZacDair <zacdair@gmail.com>
2020-09-24 20:25:15 +0100

    added unit-2 of app dev frameworks, completed as far as autowiring

---
+++ App_Dev_Frameworks/unit-2/src/main/java/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main{
+
+    public static void main(String [] args){
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        Publisher p = ((Publisher) context.getBean("dc"));
+        System.out.println(p);
+
+        Franchise av = ((Franchise) context.getBean("arrowverse"));
+        System.out.println(av);
+
+        Franchise jl = ((Franchise) context.getBean("justiceLeague"));
+        System.out.println(jl);
+
+        Hero bc = ((Hero) context.getBean("blackCanary"));
+        System.out.println(bc);
+
+        Hero ga = ((Hero) context.getBean("greenArrow"));
+        System.out.println(ga);
+
+        Hero kf = ((Hero) context.getBean("killerFrost"));
+        System.out.println(kf);
+
+        Hero ww = ((Hero) context.getBean("wonderWoman"));
+        System.out.println(ww);
+
+        Hero aqua = ((Hero) context.getBean("aquaman"));
+        System.out.println(aqua);
+
+        Hero bats = ((Hero) context.getBean("batman"));
+        System.out.println(bats);
+
+    }
+}
