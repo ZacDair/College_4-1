e2b9380a3b35af2d17c1f966cfd30f82e32d6300
ZacDair <zacdair@gmail.com>
2020-10-07 15:45:19 +0100

    Added unit-4-2, H2 database instead of SQL

---
+++ App_Dev_Frameworks/unit-4-2/target/classes/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO Hero (heroID, heroName) VALUES
+(1, 'Iron Man'),
+(2, 'Green Arrow'),
+(3, 'Thor'),
+(4, 'Flash'),
+(5, 'Spiderman'),
+(6, 'Hulk'),
+(7, 'Black Panther'),
+(8, 'Captain America'),
+(9, 'Doctor Strange');
