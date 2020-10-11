2486d72b92a4e125f6a66e22d0fd77ff5c3a3d91
ZacDair <zacdair@gmail.com>
2020-10-11 19:45:29 +0100

    added updated file from Clion

--- Embedded_Sys_Eng/lab-1-2/traffic-model.c
+++ Embedded_Sys_Eng/lab-1-2/traffic-model.c
@@ -4,9 +4,9 @@
 
 
 struct State {
-  unsigned long Out;
-  unsigned long Time; 
-  unsigned long Next[4];};
+    unsigned long Out;
+    unsigned long Time;
+    unsigned long Next[4];};
 typedef const struct State STyp;
 
 #define goN   0
@@ -15,10 +15,10 @@ typedef const struct State STyp;
 #define waitE 3
 
 STyp FSM[4]={
- {0x21,3000,{goN,waitN,goN,waitN}},
- {0x22, 500,{goE,goE,goE,goE}},
- {0x0C,3000,{goE,goE,waitE,waitE}},
- {0x14, 500,{goN,goN,goN,goN}}};
+        {0x21,3000,{goN,waitN,goN,waitN}},
+        {0x22, 500,{goE,goE,goE,goE}},
+        {0x0C,3000,{goE,goE,waitE,waitE}},
+        {0x14, 500,{goN,goN,goN,goN}}};
 
 unsigned long S;  // index to the current state
 
@@ -28,73 +28,67 @@ unsigned long Input=0;
 
 // cc.byexamples.com calls this int kbhit(), to mirror the Windows console
 //  function of the same name.  Otherwise, the code is the same.
-int inputAvailable()  
-{
-  struct timeval tv;
-  fd_set fds;
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
-  FD_ZERO(&fds);
-  FD_SET(STDIN_FILENO, &fds);
-  select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
-  return (FD_ISSET(0, &fds));
+int inputAvailable(){
+    struct timeval tv;
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    select(STDIN_FILENO+1, &fds, NULL, NULL, &tv);
+    return (FD_ISSET(0, &fds));
 }
 
-int main()
-{ 
+int main(){
     char data[200];
     int oldOutput = 0;
-
     S = goN;
     while(1){
-       usleep(100);
-       /* add in  here your emulation of the code */
+        usleep(100);
+        /* add in  here your emulation of the code */
 
-/* TODO replace the following line with a printf of the current value of lights
-        The value of the lights is in FSM[S].Out */
+        /* TODO replace the following line with a printf of the current value of lights
+            The value of the lights is in FSM[S].Out */
+        int lightVal = FSM[S].Out;
+        if(lightVal != oldOutput) {
+            if (lightVal & 1) {
+                printf("%d-1\n", lightVal);
+            } else if (lightVal & 2) {
+                printf("%d-2\n", lightVal);
+            } else if (lightVal & 3) {
+                printf("%d-3\n", lightVal);
+            } else if (lightVal & 4) {
+                printf("%d-4\n", lightVal);
+            }
+        }
 
-       int lightVal = FSM[S].Out;
-       if (lightVal & 1){
-        printf("%d-1\n",lightVal);
-       }
-       else if (lightVal & 2){
-        printf("%d-2\n",lightVal);
-       }
-       else if (lightVal & 3){
-        printf("%d-3\n",lightVal);
-       }
-       else if (lightVal & 4){
-        printf("%d-4\n",lightVal);
-       }
-      
-      
 
-  //  SysTick_Wait10ms(FSM[S].Time);   // no need of this line in initial testing
-                                     // use usleep() instead of Syst=Tick_Wait
 
-    //Input = SENSOR;             // read sensors the input value is set in the 
-                                // if statement below
+        //SysTick_Wait10ms(FSM[S].Time);   // no need of this line in initial testing
+        // use usleep() instead of Syst=Tick_Wait
+        //Input = SENSOR;             // read sensors the input value is set in the
+        // if statement below
 
-       if (inputAvailable()) {
-          Input =0;
-          scanf("%s" , data);
-          printf("you entered %s\n",data);
+        if (inputAvailable()) {
+            Input =0;
+            scanf("%s" , data);
+            printf("you entered %s\n",data);
 /* TODO  Set the input value to 00 or 01 or 10 or 11 binary i.e. 0,1,2,3 decimal depending on the input.
          For example If the user enters N set Input to 2 decimal i.e. 10 binary. */
-          if(*data == 'N'){
-            Input = 2;
-          }
-          else if (*data == 'E'){
-            Input = 3;
-          }
-          else if (data == "NE"){
-            Input = 4;
-          }
-          else{
-            Input = 1;
-          }
-         }
-      S = FSM[S].Next[Input];     // keep this line as is
-     }
+            if(*data == 'N'){
+                Input = 1;
+            }
+            else if (*data == 'E'){
+                Input = 2;
+            }
+            else if (data == "NE"){
+                Input = 3;
+            }
+            else{
+                Input = 0;
+            }
+        }
+        S = FSM[S].Next[Input];     // keep this line as is
+    }
 
 }
