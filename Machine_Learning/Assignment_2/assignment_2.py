f4e25afc612eca1b9ab1c73a35b22b603ab38d32
Zac Dair <zacdair@gmail.com>
2020-11-19 00:39:07 +0000

    added task 3 loop, encapsulated functions, minor comparison left

--- Machine_Learning/Assignment_2/assignment_2.py
+++ Machine_Learning/Assignment_2/assignment_2.py
@@ -2,10 +2,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import time
 
-from sklearn import model_selection, linear_model, metrics
+from sklearn import model_selection, linear_model, metrics, svm
 
 
-def preProcessingAndVisualization(sampleSize):
+# Prints a count for each class, and displays some examples of each class
+def displayDataInfo(features, lables):
+    # Count our labels (0 for sneaker, 1 for ankle boot)
+    sneakerCount = len(lables[lables == 0])
+    bootCount = len(lables[lables == 1])
+    print("The dataset consists of", sneakerCount, "sneakers and", bootCount, "ankle boots.")
+
+    # Display 2 of each class (currently first two, might be nice to go random)
+    plt.figure()
+    for label in range(2):
+        for figIndex in range(2):
+            plt.subplot(2, 2, label * 2 + figIndex + 1)
+            plt.imshow(features[lables == label][figIndex].reshape(28, 28))
+    plt.show()
+
+
+# Reads in our csv dataset using a size value to determine how much is retrieved (returns features and labels)
+def retrieveData(sampleSize):
     # File reading with error checking
     foundFile = False
     filename = 'product_images.csv'
@@ -30,76 +47,96 @@ def preProcessingAndVisualization(sampleSize):
         # Copy the original dataframe, minus the label column (leaves us with feature vectors)
         dataFeatures = data.drop(columns='label').to_numpy()
 
-        # Count our labels (0 for sneaker, 1 for ankle boot)
-        sneakerCount = len(dataLabels[dataLabels == 0])
-        bootCount = len(dataLabels[dataLabels == 1])
-        print("The dataset consists of", sneakerCount, "sneakers and", bootCount, "ankle boots.")
-
-        # Display 2 of each class (currently first two, might be nice to go random)
-        plt.figure()
-        for label in range(2):
-            for figIndex in range(2):
-                plt.subplot(2, 2, label * 2 + figIndex + 1)
-                plt.imshow(dataFeatures[dataLabels == label][figIndex].reshape(28, 28))
-        plt.show()
-
         return dataFeatures, dataLabels
 
 
-features, labels = preProcessingAndVisualization(0)
+# Calculates the min, max and average of a list of values
+def calculateMinMaxAvg(values):
+    return min(values), max(values), sum(values) / len(values)
+
+
+# Train and Test Model using K-folds
+def trainAndRunModel(model, featureData, labelData, kFold, verbosity):
+    # Lists to store timings and accuracy values
+    predictTimes = []
+    trainTimes = []
+    accuracies = []
+
+    # Train Test Splits for Cross Validation
+    for trainIndex, testIndex in kFold.split(featureData):
+
+        # Fit model to the data
+        trainTimeStart = time.time()
+        model.fit(featureData[trainIndex], labelData[trainIndex])
+        trainTimeEnd = time.time()
+
+        # Get predictions
+        predictionTimeStart = time.time()
+        predictions = model.predict(featureData[testIndex])
+        predictionTimeEnd = time.time()
+
+        # Get accuracy score and confusion matrix
+        score = metrics.accuracy_score(labelData[testIndex], predictions)
+        confusionMatrix = metrics.confusion_matrix(labelData[testIndex], predictions)
+        if verbosity:
+            print("\nPerceptron Accuracy:", score)
+            print("Training Time:", trainTimeEnd - trainTimeStart)
+            print("Prediction Time:", predictionTimeEnd - predictionTimeStart)
+            print("Confusion Matrix:")
+            print(confusionMatrix)
+
+        trainTimes.append(trainTimeEnd - trainTimeStart)
+        predictTimes.append(predictionTimeEnd - predictionTimeStart)
+        accuracies.append(score)
+
+    minTrainTime, maxTrainTime, avgTrainTime = calculateMinMaxAvg(trainTimes)
+    minPredictTime, maxPredictTime, avgPredictTime = calculateMinMaxAvg(predictTimes)
+    minAccuracy, maxAccuracy, avgAccuracy = calculateMinMaxAvg(accuracies)
+
+    print("\nTraining Time:")
+    print("Min:", minTrainTime, "\nMax:", maxTrainTime, "\nAvg:", avgTrainTime)
+    print("\nPrediction Time:")
+    print("Min:", minPredictTime, "\nMax:", maxPredictTime, "\nAvg:", avgPredictTime)
+    print("\nAccuracy:")
+    print("Min:", minAccuracy, "\nMax:", maxAccuracy, "\nAvg:", avgAccuracy)
+
+
+# Retrieve the full dataset
+datasetFeatures, datasetLabels = retrieveData(0)
+
+# Display info about the full dataset
+displayDataInfo(datasetFeatures, datasetLabels)
 
 # K-fold init
 kf = model_selection.KFold(n_splits=10, shuffle=True)
 
-# Lists to store timings and accuracy values
-predictTimes = []
-trainTimes = []
-accuracies = []
+# Define our perceptron model
+perceptron = linear_model.Perceptron()
 
-# Train Test Splits for Cross Validation
-for trainIndex, testIndex in kf.split(features):
-    # Initialise perceptron model
-    perceptron = linear_model.Perceptron()
+# Define various support vector machine models
+svm1 = svm.SVC(kernel='linear')
+svm2 = svm.SVC(kernel='rbf', gamma=1e-1)
+svm3 = svm.SVC(kernel='rbf', gamma=1e-2)
+svm4 = svm.SVC(kernel='rbf', gamma=1e-3)
+svm5 = svm.SVC(kernel='rbf', gamma=1e-4)
 
-    # Fit model to the data
-    trainTimeStart = time.time()
-    perceptron.fit(features[trainIndex], labels[trainIndex])
-    trainTimeEnd = time.time()
+models = [perceptron, svm1, svm2, svm3, svm4, svm5]
 
-    # Get predictions
-    predictionTimeStart = time.time()
-    predictions = perceptron.predict(features[testIndex])
-    predictionTimeEnd = time.time()
+# Loop through each different model
+for m in models:
+    # Loop through 8 different amounts of the dataset (0 means all)
+    sizeList = [10, 50, 100, 500, 1000, 5000, 10000, 0]
+    for size in sizeList:
 
-    # Get accuracy score and confusion matrix
-    score = metrics.accuracy_score(labels[testIndex], predictions)
-    confusionMatrix = metrics.confusion_matrix(labels[testIndex], predictions)
-    print("\nPerceptron Accuracy:", score)
-    print("Training Time:", trainTimeEnd - trainTimeStart)
-    print("Prediction Time:", predictionTimeEnd - predictionTimeStart)
-    print("Confusion Matrix:")
-    print(confusionMatrix)
+        # Get our new dataset (changes in size)
+        dFeatures, dLabels = retrieveData(size)
+        if type(m).__name__ == "Perceptron":
+            modelName = "Perceptron"
+        else:
+            modelName = "SVM(" + m.kernel + " Gamma: " + str(m.gamma) + ")"
+        print("\nModel:", modelName, "Running with a Dataset of", len(dLabels), "entries.")
 
-    trainTimes.append(trainTimeEnd - trainTimeStart)
-    predictTimes.append(predictionTimeEnd - predictionTimeStart)
-    accuracies.append(score)
+        # Fit and run predictions on our model
+        trainAndRunModel(m, dFeatures, dLabels, kf, False)
 
-# Calculate min, max, average train times, predict times, and accuracy
-minTrainTime = min(trainTimes)
-maxTrainTime = max(trainTimes)
-avgTrainTime = sum(trainTimes) / len(trainTimes)
-
-minPredictTime = min(predictTimes)
-maxPredictTime = max(predictTimes)
-avgPredictTime = sum(predictTimes) / len(predictTimes)
-
-minAccuracy = min(accuracies)
-maxAccuracy = max(accuracies)
-avgAccuracy = sum(accuracies) / len(accuracies)
-
-print("\n\nTraining Time:")
-print("Min:", minTrainTime, "\nMax:", maxTrainTime, "\nAvg:", avgTrainTime)
-print("\nPrediction Time:")
-print("Min:", minPredictTime, "\nMax:", maxPredictTime, "\nAvg:", avgPredictTime)
-print("\nAccuracy:")
-print("Min:", minAccuracy, "\nMax:", maxAccuracy, "\nAvg:", avgAccuracy)
+# Get mean of gamma value accuracies for svm's to disern the best svm gamme value
